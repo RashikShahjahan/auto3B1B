a601a17c0be2fa8a6966c9baabd47e844c14da2d
Rashik Shahjahan <rashikshahjahan@protonmail.com>
2024-12-06 16:25:02 -0500

    events

--- backend/src/workers/worker.factory.ts
+++ backend/src/workers/worker.factory.ts
@@ -1,5 +1,7 @@
 import { Worker } from 'bullmq';
 import type { ConnectionOptions, Processor } from 'bullmq';
+import { jobEvents } from '../server';
+
 export function createWorker(
     name: string,
     processor: Processor,
@@ -8,12 +10,22 @@ export function createWorker(
   ) {
     const worker = new Worker(name, processor, {connection, concurrency});
   
-    worker.on("completed", (job, err) => {
+    worker.on("completed", (job, returnvalue) => {
       console.log(`Completed job ${job?.id} on queue ${name}: ${job?.returnvalue ? JSON.stringify(job?.returnvalue) : 'No return value' }`);
+      jobEvents.emit('job-completed', {
+        jobId: job?.id,
+        queue: name,
+        returnvalue
+      });
     });
   
     worker.on("failed", (job, err) => {
-      console.log(`Faille job ${job?.id} on queue ${name}`, err);
+      console.log(`Failed job ${job?.id} on queue ${name}`, err);
+      jobEvents.emit('job-failed', {
+        jobId: job?.id,
+        queue: name,
+        error: err.message
+      });
     });
   
     return worker;
